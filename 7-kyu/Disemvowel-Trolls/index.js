f7a44e11c425404c37662aaf53bef1b13f0dc838
Lazaro Moreno <lazmoreno29@gmail.com>
2022-10-03 23:04:46 -0400

    removing vowels from a string

---
+++ 7-kyu/Disemvowel-Trolls/index.js
@@ -0,0 +1,22 @@
+const disemvowel = (string) => {
+	let newStr = "";
+	for (let letter of string) {
+		// console.log({letter})
+		if (!"aeiou".includes(letter.toLowerCase())) {
+			newStr += letter;
+		}
+	}
+	return newStr;
+};
+
+// function disemvowel(str) {
+//   return str.replace(/[aeiou]/gi, '');
+// }
+
+console.log(disemvowel("This website is for losers LOL!")); // "Ths wbst s fr lsrs LL!"
+console.log(
+	disemvowel(
+		"No offense but,\nYour writing is among the worst I've ever read"
+	)
+); // "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd"
+console.log(disemvowel("What are you, a communist?")); // "Wht r y,  cmmnst?"
