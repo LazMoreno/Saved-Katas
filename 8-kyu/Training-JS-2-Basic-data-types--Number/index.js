964897373db4a66d80068f6de627a0a7834fff95
Lazaro Moreno <lazmoreno29@gmail.com>
2022-10-20 20:01:47 -0400

    numbers practice

---
+++ 8-kyu/Training-JS-2-Basic-data-types--Number/index.js
@@ -0,0 +1,41 @@
+let v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+function equal1() {
+	let a = v1;
+	let b = v1;
+	return a + b;
+}
+//Please refer to the example above to complete the following functions
+function equal2() {
+	let a = v3;
+	let b = v1;
+	return a - b;
+}
+function equal3() {
+	let a = v1;
+	let b = v5;
+	return a * b;
+}
+function equal4() {
+	let a = v4;
+	let b = v5;
+	return a / b;
+}
+function equal5() {
+	let a = v6;
+	let b = v3;
+	return a % b;
+}
+
+// let v1=50,
+//     v2=100,
+//     v3=150,
+//     v4=200,
+//     v5=2,
+//     v6=250;
+
+// const equal1 = () => v1 + v1;
+// const equal2 = () => v3 - v1;
+// const equal3 = () => v1 * v5;
+// const equal4 = () => v4 / v5;
+// const equal5 = () => v2 % v4;
